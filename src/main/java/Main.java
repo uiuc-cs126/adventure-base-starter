d19d00d836457e94c54acad4914a039b288b095a
Patrick J Gallagher <patrickj@cpgallagher.com>
2021-02-10 05:27:46 -0600

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,5 @@
-import org.glassfish.grizzly.http.server.HttpServer;
-import student.server.AdventureResource;
-import student.server.AdventureServer;
-
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         // Wishing you good luck on your Adventure!
     }
 }
