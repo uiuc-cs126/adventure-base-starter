718b540c4d70c33ac608063be583bb75515265f0
Patrick J Gallagher <patrickj@cpgallagher.com>
2021-02-10 05:27:26 -0600

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        HttpServer server = AdventureServer.createServer(AdventureResource.class);
-        server.start();
+        // Wishing you good luck on your Adventure!
     }
 }
